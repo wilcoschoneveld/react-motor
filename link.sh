024273f78b6e2814a27c382855b7dc38e6a5bdb1
Wilco Schoneveld <schoneveld.wj@gmail.com>
2019-04-11 12:06:22 +0200

    Removed accidental commit of link util script

--- link.sh
+++
@@ -1,6 +0,0 @@
-yarn link
-(cd ../regminer-fe/node_modules/react && yarn link)
-(cd ../regminer-fe/node_modules/@types/react && yarn link)
-yarn link react
-yarn link @types/react
-(cd ../regminer-fe && yarn link react-motor)
